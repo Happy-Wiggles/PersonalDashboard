96251c4a56d8937bfa09d288c7f11f47cc86a61c
Justin Scheel <justin.scheel42@gmail.com>
2026-04-13 17:46:58 +0200

    Fixed a date formatting issue

--- frontend/src/components/TinyComponents/CurrentDate.tsx
+++ frontend/src/components/TinyComponents/CurrentDate.tsx
@@ -1,33 +1,31 @@
 import { useState, useEffect } from "react";
 
 const CurrentDate = () => {
-  const initialDate = new Date().toLocaleDateString("de-DE");
-  const [date, setDate] = useState<string>(initialDate);
+  const initialDate = new Date();
+  const [date, setDate] = useState<Date>(initialDate);
 
   // Basically an infinite async loop (called when date has changed => changes date => repeeat)
   useEffect(() => {
-    setTimeout(() => setDate(new Date().toLocaleDateString("de-DE")), 300000); // Every 5min
+    const timer = setTimeout(() => {
+      setDate(new Date());
+    }, 300000); // Every 5 Min
+
+    // Clear Timer => no Memory Leaks
+    return () => clearTimeout(timer);
   }, [date]);
 
-  const formatDate = (date: string | undefined) => {
-    if (date === undefined) {
-      return new Date();
-    }
-
-    const dateObj = new Date(date);
-    const formattedDate = new Intl.DateTimeFormat("de-DE", {
+  const formatDate = (dateObj: Date) => {
+    return new Intl.DateTimeFormat("de-DE", {
       day: "2-digit",
       month: "2-digit",
       year: "numeric",
     }).format(dateObj);
-
-    return formattedDate;
   };
 
   return (
     <div>
       <p className="text-2xl text-gray-200 font-bold">
-        {formatDate(date).toString()}
+        {formatDate(date)?.toString()}
       </p>
     </div>
   );
