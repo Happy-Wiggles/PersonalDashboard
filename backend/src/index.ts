583098372aedaae6d233e7e8bb11e493412466c4
Justin Scheel <justin.scheel42@gmail.com>
2026-05-13 12:23:00 +0200

    fixed cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,6 +43,8 @@ app.use(
   cors({
     origin: ALLOWED_ORIGINS,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
